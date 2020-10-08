4946187f95b23ddbd95816718ee65479dc202871
Nikita Penskoy <nik95penik@yandex.ru>
2020-10-08 16:17:37 +0300

    Add some pellet methods

--- src/main/java/com/example/demo/models/businesslogic/backend/PelletBackend.java
+++ src/main/java/com/example/demo/models/businesslogic/backend/PelletBackend.java
@@ -1,10 +1,15 @@
 package com.example.demo.models.businesslogic.backend;
 
 import com.example.demo.models.entities.Law;
+import com.example.demo.models.entities.LawFormulation;
 import com.example.demo.models.entities.Mistake;
 import com.example.demo.utils.HyperText;
+import openllet.owlapi.OpenlletReasoner;
+import openllet.owlapi.OpenlletReasonerFactory;
 import org.semanticweb.owlapi.apibinding.OWLManager;
 import org.semanticweb.owlapi.model.*;
+import org.semanticweb.owlapi.reasoner.Node;
+import org.semanticweb.owlapi.reasoner.NodeSet;
 import org.semanticweb.owlapi.vocab.OWL2Datatype;
 import org.swrlapi.core.SWRLRuleEngine;
 import org.swrlapi.exceptions.SWRLBuiltInException;
@@ -12,54 +17,48 @@ import org.swrlapi.factory.SWRLAPIFactory;
 import org.swrlapi.parser.SWRLParseException;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class PelletBackend extends SWRLBackend {
+    OpenlletReasoner Reasoner;
+
+    public PelletBackend () {
+        super();
+        Reasoner = OpenlletReasonerFactory.getInstance().createReasoner(Ontology);
+    }
+
+    public NodeSet<OWLNamedIndividual> getAllIndividuals() {
+        return Reasoner.getInstances(getThingClass());
+    }
+
+    public String getDataValue(OWLNamedIndividual ind, OWLDataProperty dataProperty) {
+        Set<OWLLiteral> data = Reasoner.getDataPropertyValues(ind, dataProperty);
+
+        if (data.size() > 1) {
+            throw new RuntimeException(ind.toStringID() + " has multiple " + dataProperty.toString() + " :" + data.toString());
+        }
+        if (data.isEmpty()) {
+            return "";
+        }
+
+        return data.iterator().next().getLiteral();
+    }
+
     @Override
-    public List<Mistake> judge(List<Law> laws, HyperText problem, List<BackendFact> statement, List<BackendFact> response) {
-        String base = "http://www.test/test.owl";
-        IRI ontologyIRI = IRI.create(base);
-        OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
-        OWLOntology ontology = null;
-        try {
-            ontology = manager.createOntology(ontologyIRI);
-        } catch (OWLOntologyCreationException e) {
-            e.printStackTrace();
-        }
-        OWLDataFactory factory = manager.getOWLDataFactory();
-        manager.getOntologyFormat(ontology).asPrefixOWLDocumentFormat().setDefaultPrefix(base + "#");
-
-        OWLClass adult = factory.getOWLClass(IRI.create(ontologyIRI + "#Adult"));
-        OWLClass person = factory.getOWLClass(IRI.create(ontologyIRI + "#Person"));
-        OWLDataProperty hasAge = factory.getOWLDataProperty(IRI.create(ontologyIRI + "#hasAge"));
-
-        OWLNamedIndividual john = factory.getOWLNamedIndividual(IRI.create(ontologyIRI + "#John"));
-        OWLNamedIndividual andrea = factory.getOWLNamedIndividual(IRI.create(ontologyIRI + "#Andrea"));
-
-        OWLClassAssertionAxiom classAssertion = factory.getOWLClassAssertionAxiom(person, john);
-        manager.addAxiom(ontology, classAssertion);
-        classAssertion = factory.getOWLClassAssertionAxiom(person, andrea);
-        manager.addAxiom(ontology, classAssertion);
-
-        OWLDatatype integerDatatype = factory.getOWLDatatype(OWL2Datatype.XSD_INTEGER.getIRI());
-        OWLLiteral literal = factory.getOWLLiteral("41", integerDatatype);
-        OWLAxiom ax = factory.getOWLDataPropertyAssertionAxiom(hasAge, andrea, literal);
-        manager.addAxiom(ontology, ax);
-
-        literal = factory.getOWLLiteral("15", integerDatatype);
-        ax = factory.getOWLDataPropertyAssertionAxiom(hasAge, john, literal);
-        manager.addAxiom(ontology, ax);
-
-        SWRLRuleEngine ruleEngine = SWRLAPIFactory.createSWRLRuleEngine(ontology);
-        try {
-            ruleEngine.createSWRLRule("r1", "Person(?p)^hasAge(?p,?age)^swrlb:greaterThan(?age,17) -> Adult(?p)");
-        } catch (SWRLParseException e) {
-            e.printStackTrace();
-        } catch (SWRLBuiltInException e) {
-            e.printStackTrace();
-        }
-
+    List<Mistake> findErrors() {
         List<Mistake> result = new ArrayList<>();
+
+        for (Node<OWLNamedIndividual> sameInd : Reasoner.getInstances(Person)) {
+            OWLNamedIndividual ind = sameInd.getRepresentativeElement();
+
+            OWLDataProperty dp = getDataProperty("hasAge");
+
+            String age = getDataValue(ind, dp);
+            Mistake m = new Mistake();
+            result.add(m);
+        }
         return result;
     }
 }
