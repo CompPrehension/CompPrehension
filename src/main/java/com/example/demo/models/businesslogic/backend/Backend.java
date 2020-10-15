025cf0a021f859ddc7a4adcb3b633644bc45814c
Nikita Penskoy <nik95penik@yandex.ru>
2020-10-15 17:43:20 +0300

    Backend interface

--- src/main/java/com/example/demo/models/businesslogic/backend/Backend.java
+++ src/main/java/com/example/demo/models/businesslogic/backend/Backend.java
@@ -7,5 +7,12 @@ import java.util.List;
 
 public abstract class Backend {
 
-    public abstract List<Mistake> judge(List<Law> laws, List<BackendFact> statement, List<BackendFact> response);
+    public abstract List<Mistake> judge(List<Law> laws, List<BackendFact> statement, List<BackendFact> correctAnswer, List<BackendFact> response);
+    public abstract List<BackendFact> solve(List<Law> laws, List<BackendFact> statement);
+
+    class BackendAnswer {
+        List<Mistake> Mistakes;
+        List<BackendFact> CorrectAswer;
+    };
+    public abstract BackendAnswer solveAndJudge(List<Law> laws, List<BackendFact> statement, List<BackendFact> response);
 }
