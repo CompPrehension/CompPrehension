7b4cc53bf9090d82c6aaa602ecfad58ad44260a5
Nikita Penskoy <nik95penik@yandex.ru>
2020-11-12 21:04:16 +0300

    Add real concepts and laws

--- src/main/java/com/example/demo/models/businesslogic/ProgrammingLanguageExpressionDomain.java
+++ src/main/java/com/example/demo/models/businesslogic/ProgrammingLanguageExpressionDomain.java
@@ -21,18 +21,50 @@ public class ProgrammingLanguageExpressionDomain extends Domain {
         concepts = new ArrayList<>();
         laws = new ArrayList<>();
 
-        concepts.add(Concept.createConcept("Basic arithmetics", domainEntity));
-        concepts.add(Concept.createConcept("Associativity", domainEntity));
-        concepts.add(Concept.createConcept("Basic logic operators", domainEntity));
-        concepts.add(Concept.createConcept("Basic assignment", domainEntity));
-        concepts.add(Concept.createConcept("Pointers", domainEntity));
-        concepts.add(Concept.createConcept("Arrays", domainEntity));
-        Concept precedence = Concept.createConcept("Precedence", domainEntity);
-        concepts.add(precedence);
+        Concept operatorConcept = addConcept("operator");
+        Concept variableConcept = addConcept("variable");
+        Concept literalConcept = addConcept("literal");
+        Concept simpleOperandConcept = addConcept("simple_operand");
+        Concept precedenceConcept = addConcept("precedence");
+        Concept associativityConcept = addConcept("associativity");
+        Concept arityConcept = addConcept("arity");
+        Concept singleTokenUnaryConcept = addConcept("single_token_unary");
+        Concept singleTokenBinaryConcept = addConcept("single_token_binary");
+        Concept twoTokenUnaryConcept = addConcept("two_token_unary");
+        Concept twoTokenBinaryConcept = addConcept("two_token_binary");
+        Concept twoTokenTernaryConcept = addConcept("two_token_ternary");
+        Concept operatorEvaluationStateConcept = addConcept("operator_evaluation_state");
+        Concept leftAssociativityConcept = addConcept("left_associativity");
+        Concept rightAssociativityConcept = addConcept("right_associativity");
+        Concept absentAssociativityConcept = addConcept("absent_associativity");
+        Concept operatorEvaluatingLeftOperandFirstConcept = addConcept("operator_evaluating_left_operand_first");
+        Concept operatorUnaryPlusConcept = addConcept("operator_unary_+");
+        Concept operatorBinaryPlusConcept = addConcept("operator_binary_+");
+        Concept operatorEqualsConcept = addConcept("operator_==");
+        Concept operatorPrefixIncrementConcept = addConcept("operator_prefix_++");
 
-        List<Concept> precedenceConcepts = new ArrayList<>();
-        precedenceConcepts.add(precedence);
-        laws.add(Law.createLaw("Less operator precedence", true, domainEntity, precedenceConcepts));
+        List<Concept> singleTokenBinaryExecutionConcepts = List.of(
+                precedenceConcept,
+                associativityConcept,
+                operatorConcept,
+                singleTokenBinaryConcept,
+                simpleOperandConcept,
+                operatorEvaluationStateConcept);
+        laws.add(Law.createLaw("single_token_binary_execution", true, domainEntity, singleTokenBinaryExecutionConcepts));
+
+        List<Concept> errorSingleTokenBinaryOperatorHasUnevaluatedHigherPrecedenceLeft = List.of(
+                precedenceConcept,
+                operatorConcept,
+                singleTokenBinaryConcept,
+                simpleOperandConcept
+        );
+        laws.add(Law.createLaw("error_single_token_binary_operator_has_unevaluated_higher_precedence_left", false, domainEntity, errorSingleTokenBinaryOperatorHasUnevaluatedHigherPrecedenceLeft));
+    }
+
+    private Concept addConcept(String name) {
+        Concept concept = Concept.createConcept(name, domainEntity);
+        concepts.add(concept);
+        return concept;
     }
 
     static DomainEntity create(DomainService domainService) {
@@ -63,7 +95,7 @@ public class ProgrammingLanguageExpressionDomain extends Domain {
     @Override
     public Question makeQuestion(QuestionRequest questionRequest, Language userLanguage) {
         for (Concept concept : questionRequest.getTargetConcepts()) {
-            if (concept.getName().equals("Pointers")) {
+            if (concept.getName().equals("associativity")) {
                 com.example.demo.models.entities.Question question = new com.example.demo.models.entities.Question();
                 question.setQuestionText("* * b");
                 question.setQuestionType(QuestionType.ORDER);
@@ -72,7 +104,7 @@ public class ProgrammingLanguageExpressionDomain extends Domain {
         }
 
         com.example.demo.models.entities.Question question = new com.example.demo.models.entities.Question();
-        question.setQuestionText("a + b + c");
+        question.setQuestionText("a + b * c");
         question.setQuestionType(QuestionType.SINGLE_CHOICE);
         return new SingleChoice(question);
     }
