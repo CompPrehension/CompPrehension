e47d00760c54d688b3683e9fe67042037cab825a
prokudin <prokudintema@yandex.ru>
2021-03-11 22:16:00 +0300

    add more declarative ajax requests syntax

--- src/main/js/client/utils/ajax.ts
+++ src/main/js/client/utils/ajax.ts
@@ -1,29 +1,27 @@
-import { Either } from "fp-ts/lib/Either";
-import * as TE from "fp-ts/lib/TaskEither";
+import { Fetcher } from "fetcher-ts";
+import { Either, left, right } from "fp-ts/lib/Either";
+import { fst } from "fp-ts/lib/Tuple";
 
 type RequestError = {
-    error: string,
+    error?: string,
     message: string,
-    path: string,
-    status: number,
-    timestamp: string,
-    trace: string,
+    path?: string,
+    status?: number,
+    timestamp?: string,
+    trace?: string,
 }
-
 
 export function ajaxGet<T>(url: string) : Promise<Either<RequestError, T>> {
     console.log(`ajax get: ${url}`);
-    const lazyPromise = async () => {
-        const resp = await fetch(url);
-        const json = await resp.json();
-        if (resp.ok) {
-            console.log(json);
-            return json;
-        }
-        console.error(json);
-        throw json;
-    };
-    return TE.tryCatch(lazyPromise, rej => rej as RequestError)();
+
+    type FetcherResults = 
+        | { code: 200, payload: T } 
+        | { code: 500, payload: RequestError };
+    const fetcher = new Fetcher<FetcherResults, Either<RequestError, T>>(url)
+        .handle(200, data => (console.log(data), right(data)))
+        .handle(500, error => (console.error(error), left(error)))
+        .discardRest(() => (console.error("Unhandled request error"), left({ message: "Unhandled request error" })));
+    return fetcher.run().then(fst);
 }
 
 export function ajaxPost<T>(url: string, body: object) : Promise<Either<RequestError, T>> {
@@ -36,15 +34,12 @@ export function ajaxPost<T>(url: string, body: object) : Promise<Either<RequestE
     };
 
     console.log(`ajax post: ${url}`, params.body);
-    const lazyPromise = async () => {
-        const resp = await fetch(url, params);
-        const json = await resp.json();
-        if (resp.ok) {
-            console.log(json);
-            return json;
-        }
-        console.error(json);
-        throw json;
-    };
-    return TE.tryCatch(lazyPromise, rej => rej as RequestError)();
+    type FetcherResults = 
+        | { code: 200, payload: T } 
+        | { code: 500, payload: RequestError };
+    const fetcher = new Fetcher<FetcherResults, Either<RequestError, T>>(url, params)
+        .handle(200, data => (console.log(data), right(data)))
+        .handle(500, error => (console.error(error), left(error)))
+        .discardRest(() => (console.error("Unhandled request error"), left({ message: "Unhandled request error" })));
+    return fetcher.run().then(fst);
 }
