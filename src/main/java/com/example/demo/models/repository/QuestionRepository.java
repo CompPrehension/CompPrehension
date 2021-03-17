2a73dc995b7437dedeea37517697d7183c08deba
Nikita Penskoy <nik95penik@yandex.ru>
2021-03-17 14:57:08 +0300

    remove example

--- src/main/java/com/example/demo/models/repository/QuestionRepository.java
+++ src/main/java/com/example/demo/models/repository/QuestionRepository.java
@@ -6,26 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface QuestionRepository extends CrudRepository<QuestionEntity, Long> {
-//    public QuestionEntity findQuestion(HashSet<String> target, HashSet<String> allowed, HashSet<String> denied) {
-//        @Query("SELECT a FROM A a WHERE a.userGroups not in (:userGroups)")
-//        List<A> getAWithNoIntersectionInGroups(@param("userGroups") Set<UserGroup> userGroups)
-//
-//        CriteriaBuilder builder = em.getCriteriaBuilder();
-//        CriteriaQuery<QuestionEntity> query = builder.createQuery(QuestionEntity.class);
-//        Root<QuestionEntity> root = query.from(QuestionEntity.class);
-//
-//        ParameterExpression<String> conceptParam = builder.parameter(String.class);
-//        query.select(root).where(builder.greaterThan(root.get(QuestionEntity.recipes).get(QuestionEntity.dateModified), dateParam));
-//
-//        CriteriaBuilder.In<String> inClause = builder.in(root.get("name"));
-//        for (String title : target) {
-//            inClause.value(title);
-//        }
-//        criteriaQuery.select(root).where(inClause);
-//
-//        return em.createQuery(query)
-//                .setParameter(dateParam, dateModified)
-//                .getSingleResult();
-//    }
 }
 
