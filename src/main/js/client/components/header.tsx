81e152f42ba1d18dc40869582b1d8f2aa9d1142a
prokudin <prokudintema@yandex.ru>
2021-04-06 23:06:59 +0300

    add question number displaying

--- src/main/js/client/components/header.tsx
+++ src/main/js/client/components/header.tsx
@@ -2,18 +2,21 @@ import { observer } from 'mobx-react';
 import * as React from 'react';
 import { Navbar } from 'react-bootstrap';
 import store from '../store';
+import { Optional } from './optional';
 import { Pagination } from './pagination';
 
 
 export const Header = observer(() => {
-    const { sessionInfo } = store;
+    const { sessionInfo, } = store;
     if (!sessionInfo) {
         return null;
     }
     const { user } = sessionInfo;
+    const currentQuestionIdx = sessionInfo.questionIds.findIndex(id => store.questionData?.questionId === id);
 
     return (
-        <Navbar>
+        <Navbar className="px-0">
+            <Optional condition={currentQuestionIdx !== -1}><h5>Question #{currentQuestionIdx + 1}</h5></Optional>            
             <Navbar.Collapse className="justify-content-end">   
                 <Pagination />
                 
--- src/main/js/client/components/optional.tsx
+++ src/main/js/client/components/optional.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 
-export const Optional = (props: { condition: boolean, children?: React.ReactNode[] }) => {
+export const Optional = (props: { condition: boolean, children?: React.ReactNode[] | React.ReactNode }) => {
     const { condition, children } = props;
     if (!condition) {
         return null;        
