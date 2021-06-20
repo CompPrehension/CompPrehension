5c69549dc8da6b7b6403b83e616c81471b017121
prokudin <prokudintema@yandex.ru>
2021-06-20 10:45:02 +0300

    add new pagination format

--- src/main/js/components/exercise/pagination.tsx
+++ src/main/js/components/exercise/pagination.tsx
@@ -4,26 +4,34 @@ import { useState } from 'react';
 import { Pagination as BootstrapPagination } from "react-bootstrap"
 import { container } from "tsyringe";
 import { ExerciseStore } from "../../stores/exercise-store";
+import { Optional } from '../common/optional';
 
 export const Pagination = observer(() => {
     const [exerciseStore] = useState(() => container.resolve(ExerciseStore));
-    if (!(exerciseStore.currentAttempt?.questionIds.length) || !exerciseStore.currentQuestion) {
+    if (!exerciseStore.currentQuestion.question || !exerciseStore.currentAttempt) {
         return null;
     }
+
+    const { questionId:currentQuestionId } = exerciseStore.currentQuestion.question;
+    const { questionIds } = exerciseStore.currentAttempt;
 
     return (
         <div className="d-flex justify-content-center">
             <BootstrapPagination className="p-3" style={{ marginBottom: '0 !important' }}>
-                {/*<BootstrapPagination.First />*/}
-                {/*<BootstrapPagination.Prev />*/}
-                {exerciseStore.currentAttempt?.questionIds.map((id, idx) => 
+                <Optional isVisible={questionIds.length >= 5}>
+                    <BootstrapPagination.First disabled={currentQuestionId === questionIds[0]} onClick={() => exerciseStore.currentQuestion.loadQuestion(questionIds[0])}/>
+                    <BootstrapPagination.Prev disabled={currentQuestionId === questionIds[0]} onClick={() => exerciseStore.currentQuestion.loadQuestion(questionIds[questionIds.indexOf(currentQuestionId) - 1])}/>
+                </Optional>
+                {questionIds.map((id, idx) => 
                     (<BootstrapPagination.Item key={idx + 1} 
-                                               active={exerciseStore.currentQuestion.question?.questionId === id}
+                                               active={currentQuestionId === id}
                                                onClick={() => exerciseStore.currentQuestion.loadQuestion(id)}>
                         {idx + 1}
                      </BootstrapPagination.Item>))}
-                {/*<BootstrapPagination.Next />*/}
-                {/*<BootstrapPagination.Last />*/}
+                <Optional isVisible={questionIds.length >= 5}>
+                    <BootstrapPagination.Next disabled={currentQuestionId === questionIds[questionIds.length - 1]} onClick={() => exerciseStore.currentQuestion.loadQuestion(questionIds[questionIds.indexOf(currentQuestionId) + 1])}/>
+                    <BootstrapPagination.Last disabled={currentQuestionId === questionIds[questionIds.length - 1]} onClick={() => exerciseStore.currentQuestion.loadQuestion(questionIds[questionIds.length - 1])}/>
+                </Optional>
             </BootstrapPagination>
         </div>
     );
