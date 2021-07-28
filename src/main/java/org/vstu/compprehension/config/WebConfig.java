9466ff6f00e498f696c09d9d8143d4f369550808
prokudin <prokudintema@yandex.ru>
2021-07-28 18:16:18 +0300

    add setting SameSite=None for cookies

--- src/main/java/org/vstu/compprehension/config/WebConfig.java
+++ src/main/java/org/vstu/compprehension/config/WebConfig.java
@@ -1,10 +1,23 @@
 package org.vstu.compprehension.config;
 
+import org.apache.tomcat.util.http.Rfc6265CookieProcessor;
+import org.apache.tomcat.util.http.SameSiteCookies;
+import org.springframework.boot.web.embedded.tomcat.TomcatContextCustomizer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Bean
+    public TomcatContextCustomizer sameSiteCookiesConfig() {
+        return context -> {
+            final Rfc6265CookieProcessor cookieProcessor = new Rfc6265CookieProcessor();
+            cookieProcessor.setSameSiteCookies(SameSiteCookies.NONE.getValue());
+            context.setCookieProcessor(cookieProcessor);
+        };
+    }
     /*
     @Bean
     public ResourceBundleMessageSource messageSource() {
