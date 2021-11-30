29e8ac7a50cf205038150351eea9237a7504649d
Artem Prokudin <prokudintema@yandex.ru>
2021-11-30 14:47:28 +0300

    move answer completion to server-side for order question (#175)

--- src/main/java/org/vstu/compprehension/dto/InteractionDto.java
+++ src/main/java/org/vstu/compprehension/dto/InteractionDto.java
@@ -1,11 +1,14 @@
 package org.vstu.compprehension.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.extern.jackson.Jacksonized;
 
 @Data
 @Builder @Jacksonized
+@AllArgsConstructor @NoArgsConstructor
 public class InteractionDto {
     private Long attemptId;
     private Long questionId;
