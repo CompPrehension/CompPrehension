68d36b1f3f1a98c33ec6ca2a0de7ed6b62b79cc9
den1s0v <hello-lumen@ya.ru>
2022-01-05 18:20:07 +0300

    Improve RemoteFileService: optional use of HTTP for faster download

--- src/main/java/org/vstu/compprehension/models/businesslogic/storage/RemoteFileService.java
+++ src/main/java/org/vstu/compprehension/models/businesslogic/storage/RemoteFileService.java
@@ -3,11 +3,16 @@ package org.vstu.compprehension.models.businesslogic.storage;
 import org.apache.commons.vfs2.*;
 import org.apache.commons.vfs2.util.Cryptor;
 import org.apache.commons.vfs2.util.CryptorFactory;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
+import java.util.zip.CRC32;
+import java.util.zip.Checksum;
 
 import static java.lang.Math.min;
 
@@ -16,16 +21,54 @@ public class RemoteFileService {
     final static String PATH_SEPARATORS = "/\\#;:";
     final static String PATH_LEGAL_CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789._-";
     final static String[] PATH_ILLEGAL_NAMES = {"CON", "PRN", "AUX", "NUL", "COM1", "COM2", "COM3", "COM4", "COM5", "COM6", "COM7", "COM8", "COM9", "LPT1", "LPT2", "LPT3", "LPT4", "LPT5", "LPT6", "LPT7", "LPT8", "LPT9",};  // in Windows
+    final static int BUFFER_SIZE = 100 * 1024;
 
     private final FileSystemManager mgr;
-    private final String baseUri;
+    private final String baseUploadUri;
+    private final String baseDownloadUri;
+    private int dummyDirsForNewFile = 1;
 
+    /**
+     * Create file service with upload and download endpoints, possibly different. E.g. FTP to create files and HTTP to download them faster than ordinal FTP.
+     * @param baseUploadUri URL to base dir to store data via writable protocol
+     * @param baseDownloadUri URL to base dir to read data via readable protocol
+     */
+    RemoteFileService(String baseUploadUri, String baseDownloadUri) {
+        this.baseUploadUri   = ensureTrailingSlash(baseUploadUri);
+        this.baseDownloadUri = ensureTrailingSlash(baseDownloadUri);
+        mgr = getFSManager();
+    }
+
+    /**
+     * Create file service with endpoint for both upload and download files.
+     * @param baseUri URL to base dir to store/read data via both writable and readable protocol
+     */
     RemoteFileService(String baseUri) {
+        this.baseUploadUri = ensureTrailingSlash(baseUri);
+        // copy as not specified:
+        baseDownloadUri = this.baseUploadUri;
+        mgr = getFSManager();
+    }
+
+    @NotNull
+    private static String ensureTrailingSlash(String baseUri) {
         // ensure '/' at the end of base path
+        if (
+            baseUri.charAt(baseUri.length() - 1) == '/'
+                &&
+            PATH_SEPARATORS.indexOf(baseUri.charAt(baseUri.length() - 2)) == -1
+        ) {
+            return baseUri;
+        }
+
         while (PATH_SEPARATORS.indexOf(baseUri.charAt(baseUri.length() - 1)) != -1)
             baseUri = baseUri.substring(0, baseUri.length() - 1);
-        this.baseUri = baseUri + '/';
+        baseUri = baseUri + '/';
+        return baseUri;
+    }
 
+    @Nullable
+    private static FileSystemManager getFSManager() {
         // create generic File System manager
         FileSystemManager fsManager;
         try {
@@ -37,7 +80,7 @@ public class RemoteFileService {
             //// throw e;
         }
 
-        mgr = fsManager;
+        return fsManager;
     }
 
     /** Obtain InputStream of content of specified file.
@@ -46,16 +89,16 @@ public class RemoteFileService {
      * @throws FileSystemException on communication error
      */
     InputStream getFileStream(String localName) throws FileSystemException {
-        final FileObject file = mgr.resolveFile(baseUri + localName);
+        final FileObject file = mgr.resolveFile(baseDownloadUri + localName);
         if (file.getType() != FileType.FILE || !file.isReadable()) {  // `!file.exists()` can be true for a directory
             return null;
         }
         FileContent fc = file.getContent();
-        return fc.getInputStream();
+        return fc.getInputStream(BUFFER_SIZE);
     }
 
     public boolean exists(String localName) throws FileSystemException {
-        final FileObject file = mgr.resolveFile(baseUri + localName);
+        final FileObject file = mgr.resolveFile(baseUploadUri + localName);
         return file.exists();
     }
 
@@ -65,23 +108,54 @@ public class RemoteFileService {
      * @throws FileSystemException on communication error
      */
     OutputStream saveFileStream(String localName) throws FileSystemException {
-        final FileObject file = mgr.resolveFile(baseUri + localName);
+        final FileObject file = mgr.resolveFile(baseUploadUri + localName);
         if (file.getType() == FileType.FOLDER || !file.isWriteable()) {
             return null;
         }
         FileContent fc = file.getContent();
-        return fc.getOutputStream();
+        return fc.getOutputStream();  // `BUFFER_SIZE` does not speed up
+    }
+
+    /** `When dummyDirs > 0`, for each new file of form `dir1/dir2/xxx.yyy` additional dirs inserted, e.g. `dir1/dir2/5/a/xxx.yyy` if `dummyDirs == 2`.
+     * Names of additional dirs chosen randomly out of [0-9a-f] (16 hex digits).
+     * @param dummyDirs number of dirs to create; set 0 to disable
+     */
+    public void setDummyDirsForNewFile(int dummyDirs) {
+        this.dummyDirsForNewFile = dummyDirs;
+    }
+
+    public String insertDummyDirs(String filepath) {
+        if (dummyDirsForNewFile <= 0)
+            return filepath;
+
+
+        long hash = getCRC32Checksum(filepath.getBytes(StandardCharsets.UTF_8));
+
+        StringBuilder dummy = new StringBuilder();
+        for (int i = 0; i < dummyDirsForNewFile && hash > 0; ++i) {
+            String folderName = String.format("%x", hash & 0x0f);
+            hash >>= 4;  // 0x0f is 4 bits
+            dummy.append(folderName).append("/");
+        }
+
+        int slashIndex = filepath.lastIndexOf('/');
+
+        return filepath.substring(0, slashIndex + 1) + dummy + filepath.substring(slashIndex + 1);
     }
 
     /**
      * Change name in case of clash with existing names in FS
      * @param name local file name (or relative path) to check
      * @return name corrected if necessary
-     * @throws FileSystemException on FS communication error
      */
-    public String prepareNameForNewFile(String name) {
+    public String prepareNameForFile(String name, boolean makeUnique) {
         try {
-            return uniqualizeName(normalizeName(name));
+            name = normalizeName(name);
+            name = insertDummyDirs(name);
+            if (makeUnique) {
+                name = uniqualizeName(name);
+            }
+            return name;
         } catch (FileSystemException e) {
             e.printStackTrace();
             return null;
@@ -168,6 +242,11 @@ public class RemoteFileService {
         return whole.toString();
     }
 
+    public static long getCRC32Checksum(byte[] bytes) {
+        Checksum crc32 = new CRC32();
+        crc32.update(bytes, 0, bytes.length);
+        return crc32.getValue();
+    }
 
     /**
      * Prepare connection string with optionally encrypted password to cut from output and save elsewhere
@@ -205,17 +284,39 @@ public class RemoteFileService {
         System.out.println(s);
 
 ////        String connectionStr = "file:///c:/Temp2/data/";
-//        String connectionStr = "ftp://poas:{6689596D2347FA1287A4FD6AB36AA9C8}@vds84.server-1.biz/ftp_dir/";
-//        RemoteFileService rfs = new RemoteFileService(connectionStr);
+//        String connectionStr = "ftp://poas:{6689596D2347FA1287A4FD6AB36AA9C8}@vds84.server-1.biz/ftp_dir/compp/tmp";
+//        String connectionDownloadStr = "http://vds84.server-1.biz/misc/ftp/compp/tmp/";
+//        RemoteFileService rfs = new RemoteFileService(connectionStr, connectionDownloadStr);
 //        rfs.test();
     }
 
     private void test() throws IOException {
         assert mgr != null;
 
-        String s = "1__memcpy_s";
-        String s2 = normalizeName(s);
-        System.out.println(s2);
+//        setDummyDirsForNewFile(3);
+//        System.out.println(insertDummyDirs("file1.txt"));
+//        System.out.println(insertDummyDirs("mydir/file1.txt"));
+//        System.out.println(insertDummyDirs("my/dir/file1.txt"));
+
+        // test uploads / downloads
+        for (int i = 0 ; i < 3 ; i++) {
+            long startTime = System.nanoTime();
+
+//            try(OutputStream stream = saveFileStream("dump.bin")) {
+//                for (int k = 0 ; k < 1000 ; k++) {
+//                    stream.write(new byte[100]);
+//                }
+//            }  // 1,7 s
+
+            try(InputStream stream = getFileStream("dump.bin")) {
+                while (stream.read(new byte[100]) > 0) {}
+            }  // 0.98 s via FTP ;  ~ 0.45 s via HTTP (Nginx)
+               // 0.98 s via FTP ;  ~ 0.20 s via HTTP (fast 4G)
+
+            long estimatedTime = System.nanoTime() - startTime;
+            System.out.println("Take "+ i +": " + String.format("%.5f", (float)estimatedTime / 1000 / 1000 / 1000) + " seconds.");
+        }
+
 
 //        final FileObject file = mgr.resolveFile(baseUri + "compp/a/test.abc");
 //
