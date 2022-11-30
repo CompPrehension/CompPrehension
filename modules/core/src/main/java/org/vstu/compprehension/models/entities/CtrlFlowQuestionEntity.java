35668769d12143c0cfbcfdc196bbc6be7f01277b
den1s0v <hello-lumen@ya.ru>
2022-11-30 14:30:17 +0300

    Reduce boilerplate code using Lombok

--- modules/core/src/main/java/org/vstu/compprehension/models/entities/CtrlFlowQuestionEntity.java
+++ modules/core/src/main/java/org/vstu/compprehension/models/entities/CtrlFlowQuestionEntity.java
@@ -1,7 +1,11 @@
 package org.vstu.compprehension.models.entities;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 
+@Getter @Setter
 @Entity
 @Table(name = "ctfl_questions")
 public class CtrlFlowQuestionEntity implements QuestionMetadataBaseEntity {
@@ -51,128 +55,4 @@ public class CtrlFlowQuestionEntity implements QuestionMetadataBaseEntity {
     @Column(name = "_version")
     private Integer version;
 
-    public Integer getVersion() {
-        return version;
-    }
-
-    public void setVersion(Integer version) {
-        this.version = version;
-    }
-
-    @Override
-    public Integer getStage() {
-        return stage;
-    }
-
-    public void setStage(Integer stage) {
-        this.stage = stage;
-    }
-
-    @Override
-    public Integer getDistinctErrorsCount() {
-        return distinctErrorsCount;
-    }
-
-    public void setDistinctErrorsCount(Integer distinctErrorsCount) {
-        this.distinctErrorsCount = distinctErrorsCount;
-    }
-
-    @Override
-    public Integer getSolutionSteps() {
-        return solutionSteps;
-    }
-
-    public void setSolutionSteps(Integer solutionSteps) {
-        this.solutionSteps = solutionSteps;
-    }
-
-    @Override
-    public Double getIntegralComplexity() {
-        return integralComplexity;
-    }
-
-    public void setIntegralComplexity(Double integralComplexity) {
-        this.integralComplexity = integralComplexity;
-    }
-
-    @Override
-    public Double getSolutionStructuralComplexity() {
-        return solutionStructuralComplexity;
-    }
-
-    public void setSolutionStructuralComplexity(Double solutionStructuralComplexity) {
-        this.solutionStructuralComplexity = solutionStructuralComplexity;
-    }
-
-    @Override
-    public Integer getViolationBits() {
-        return violationBits;
-    }
-
-    public void setViolationBits(Integer violationBits) {
-        this.violationBits = violationBits;
-    }
-
-    @Override
-    public Integer getLawBits() {
-        return lawBits;
-    }
-
-    public void setLawBits(Integer lawBits) {
-        this.lawBits = lawBits;
-    }
-
-    @Override
-    public Integer getConceptBits() {
-        return conceptBits;
-    }
-
-    public void setConceptBits(Integer conceptBits) {
-        this.conceptBits = conceptBits;
-    }
-
-    @Override
-    public Integer getTagBits() {
-        return tagBits;
-    }
-
-    public void setTagBits(Integer tagBits) {
-        this.tagBits = tagBits;
-    }
-
-    @Override
-    public String getQDataGraph() {
-        return qDataGraph;
-    }
-
-    public void setQDataGraph(String qDataGraph) {
-        this.qDataGraph = qDataGraph;
-    }
-
-    @Override
-    public Integer getTemplateId() {
-        return templateId;
-    }
-
-    public void setTemplateId(Integer templateId) {
-        this.templateId = templateId;
-    }
-
-    @Override
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    @Override
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
 }
