72d1001b2b133395df9973cbc5dab5d48775d78f
den1s0v <hello-lumen@ya.ru>
2023-02-13 17:25:18 +0300

    Use Singleton-scope cache for caching JenaBackend decorator

--- modules/server/src/main/java/org/vstu/compprehension/config/DiConfig.java
+++ modules/server/src/main/java/org/vstu/compprehension/config/DiConfig.java
@@ -1,5 +1,7 @@
 package org.vstu.compprehension.config;
 
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
@@ -10,8 +12,10 @@ import org.springframework.web.context.annotation.SessionScope;
 import org.vstu.compprehension.Service.UserService;
 import org.vstu.compprehension.adapters.*;
 import org.vstu.compprehension.models.businesslogic.backend.*;
+import org.vstu.compprehension.models.businesslogic.backend.facts.JenaFactList;
 import org.vstu.compprehension.models.repository.UserRepository;
 
+import javax.inject.Singleton;
 import java.util.List;
 
 @Configuration
@@ -19,12 +23,16 @@ public class DiConfig {
     @Bean
     @Qualifier("allBackends")
     @RequestScope
-    List<Backend> getAllBackends(@Autowired @Lazy JenaBackend jenaBackend, @Autowired @Lazy PelletBackend pelletBackend, @Autowired BackendTaskQueue taskQueue) {
+    List<Backend> getAllBackends(
+            @Autowired @Lazy JenaBackend jenaBackend,
+            @Autowired @Lazy PelletBackend pelletBackend,
+            @Autowired BackendTaskQueue taskQueue,
+		    @Autowired Cache<String, JenaFactList> jenaCache) {
         return List.of(
-              new RateLimitBackendDecorator(JenaBackend.BackendId, new SolutionCachingBackendDecorator(
-                      jenaBackend,
-                      "maximumSize=30,expireAfterAccess=10m"
-              ), taskQueue),
+              new RateLimitBackendDecorator(
+                      JenaBackend.BackendId,
+                      new SolutionCachingJenaBackendDecorator(jenaBackend, jenaCache),
+                      taskQueue),
               new RateLimitBackendDecorator(PelletBackend.BackendId, pelletBackend, taskQueue)
         );
     }
@@ -33,5 +41,11 @@ public class DiConfig {
     @SessionScope
     UserService getUserService(@Autowired UserRepository userRepository) {
         return new CachedUserService(new UserServiceImpl(userRepository));
+    }
+
+    @Bean
+    @Singleton
+    Cache<String, JenaFactList> getJenaSolveCache() {
+        return CacheBuilder.from("maximumSize=30,expireAfterAccess=10m").build();
     }
 }
