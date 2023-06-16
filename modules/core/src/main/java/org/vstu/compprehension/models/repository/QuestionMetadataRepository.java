1527e3b81a4437c0f4630aa9401c61bd5ccb11e1
den1s0v <hello-lumen@ya.ru>
2023-06-17 01:37:43 +0300

    Weaken condition while counting questions for a QR

--- modules/core/src/main/java/org/vstu/compprehension/models/repository/QuestionMetadataRepository.java
+++ modules/core/src/main/java/org/vstu/compprehension/models/repository/QuestionMetadataRepository.java
@@ -75,10 +75,10 @@ public interface QuestionMetadataRepository extends CrudRepository<QuestionMetad
             "AND q.concept_bits & :#{#qr.conceptsDeniedBitmask} = 0 " +
             "AND q.violation_bits & :#{#qr.lawsDeniedBitmask} = 0 " +
 
-            // at least one targeted concept and one targeted law must present
-            "   AND IF(:#{#qr.traceConceptsTargetedBitmask} =0,1,q.trace_concept_bits & :#{#qr.traceConceptsTargetedBitmask} <> 0) " +
-            "   AND IF(:#{#qr.conceptsTargetedBitmask} =0,1,q.concept_bits & :#{#qr.conceptsTargetedBitmask} <> 0) " +
-            "   AND IF(:#{#qr.lawsTargetedBitmask} =0,1,q.violation_bits & :#{#qr.lawsTargetedBitmask} <> 0) " +
+            // at least one targeted concept or one targeted law must present
+            "   AND (1 <= IF(:#{#qr.traceConceptsTargetedBitmask} =0,0,q.trace_concept_bits & :#{#qr.traceConceptsTargetedBitmask} <> 0) " +
+            "     + IF(:#{#qr.conceptsTargetedBitmask} =0,0,q.concept_bits & :#{#qr.conceptsTargetedBitmask} <> 0) " +
+            "     + IF(:#{#qr.lawsTargetedBitmask} =0,0,q.violation_bits & :#{#qr.lawsTargetedBitmask} <> 0)) " +
 
             "AND q.template_id NOT IN :#{#qr.deniedQuestionTemplateIds} " +  // note: must be non-empty
             "AND q.id NOT IN :#{#qr.deniedQuestionMetaIds}" + // note: must be non-empty
