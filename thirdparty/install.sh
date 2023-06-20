084544dcec00e79615ee086dff2392c2f1434b44
den1s0v <hello-lumen@ya.ru>
2023-06-20 13:52:27 +0300

    Windows version of install.sh command

--- thirdparty/install.sh
+++ thirdparty/install.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 mvn install:install-file -Dfile=questionGen-1.0.jar -DgroupId=com.github.max_person.its -DartifactId=questionGen -Dversion=1.0 -Dpackaging=jar
+
+# mvn install:install-file -Dfile="./thirdparty/questionGen-1.0.jar" -DgroupId="com.github.max_person.its" -DartifactId=questionGen -Dversion="1.0" -Dpackaging=jar
+
