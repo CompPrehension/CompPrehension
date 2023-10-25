ae6039152f4443033472c8d4aef019e69a182e80
Artem Prokudin <prokudintema@yandex.ru>
2023-10-25 17:20:00 +0300

    fix db provider for jobrunner

--- modules/background-server/src/main/java/org/vstu/compprehension/BackgroundServerMain.java
+++ modules/background-server/src/main/java/org/vstu/compprehension/BackgroundServerMain.java
@@ -1,9 +1,11 @@
 package org.vstu.compprehension;
 
 import org.jobrunr.configuration.JobRunr;
+import org.jobrunr.jobs.mappers.JobMapper;
 import org.jobrunr.scheduling.JobScheduler;
 import org.jobrunr.server.JobActivator;
 import org.jobrunr.storage.InMemoryStorageProvider;
+import org.jobrunr.storage.StorageProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -15,12 +17,19 @@ public class BackgroundServerMain {
     }
 
     @Bean
-    public JobScheduler initJobRunr(JobActivator jobActivator) {
+    public JobScheduler initJobRunr(JobActivator jobActivator, StorageProvider storageProvider) {
         return JobRunr.configure()
                 .useJobActivator(jobActivator)
-                .useStorageProvider(new InMemoryStorageProvider())
+                .useStorageProvider(storageProvider)
                 .useBackgroundJobServer()
                 .useDashboard()
                 .initialize().getJobScheduler();
+    }
+
+    @Bean
+    public StorageProvider storageProvider(JobMapper jobMapper) {
+        InMemoryStorageProvider storageProvider = new InMemoryStorageProvider();
+        storageProvider.setJobMapper(jobMapper);
+        return storageProvider;
     }
 }
