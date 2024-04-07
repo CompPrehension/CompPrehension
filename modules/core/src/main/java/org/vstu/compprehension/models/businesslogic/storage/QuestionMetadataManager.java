927edd2b2c45a6ea3f06e993c40f89ef3edbd904
Artem Prokudin <prokudintema@yandex.ru>
2024-04-07 15:54:29 +0300

    auto-update bank complexity stats

--- modules/core/src/main/java/org/vstu/compprehension/models/businesslogic/storage/QuestionMetadataManager.java
+++ modules/core/src/main/java/org/vstu/compprehension/models/businesslogic/storage/QuestionMetadataManager.java
@@ -1,30 +1,35 @@
 package org.vstu.compprehension.models.businesslogic.storage;
 
 import lombok.extern.log4j.Log4j2;
+import org.jetbrains.annotations.NotNull;
 import org.vstu.compprehension.models.businesslogic.storage.stats.NumericStat;
 import org.vstu.compprehension.models.repository.QuestionMetadataRepository;
 
-import java.util.HashMap;
+import java.time.LocalDateTime;
 import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Log4j2
 public class QuestionMetadataManager {
     private final QuestionMetadataRepository questionRepository;
-    private final HashMap<String, NumericStat> complexityStats;
+    private final ConcurrentHashMap<String, ComplexityStats> complexityStats;
+    
+    private record ComplexityStats(LocalDateTime createDate, NumericStat stats) {}
 
     public QuestionMetadataManager(QuestionMetadataRepository questionMetadataRepository) {
         this.questionRepository = questionMetadataRepository;
-        this.complexityStats    = new HashMap<>();
+        this.complexityStats    = new ConcurrentHashMap<>();
     }
 
     public NumericStat getComplexityStats(String domainShortname) {
-        ensureBankStatLoaded(domainShortname);
-        return complexityStats.get(domainShortname);
+        return ensureBankStatLoaded(domainShortname).stats();
     }
 
-    private void ensureBankStatLoaded(String domainShortname) {
-        if (complexityStats.containsKey(domainShortname)) {
-            return;
+    private @NotNull ComplexityStats ensureBankStatLoaded(String domainShortname) {
+        var nowShifted   = LocalDateTime.now().plusMinutes(-15);
+        var currentStats = complexityStats.get(domainShortname);
+        if (currentStats != null && currentStats.createDate().isAfter(nowShifted)) {
+            return currentStats;
         }
 
         var stats = questionRepository.getStatOnComplexityField(domainShortname);
@@ -34,6 +39,8 @@ public class QuestionMetadataManager {
                 Optional.ofNullable(stats.getMean()).orElse(0.5),
                 Optional.ofNullable(stats.getMax()).orElse(1.0)
         );
-        this.complexityStats.put(domainShortname, complexityStats);
+        var newStats = new ComplexityStats(LocalDateTime.now(), complexityStats);
+        this.complexityStats.put(domainShortname, newStats);
+        return newStats;
     }
 }
