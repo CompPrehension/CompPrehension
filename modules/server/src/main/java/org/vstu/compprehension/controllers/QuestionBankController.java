2cc4715c7e45c4817d68c64715cf75cea0fb15c7
Artem Prokudin <prokudintema@yandex.ru>
2024-04-07 15:24:57 +0300

    stats only for authorized users

--- modules/server/src/main/java/org/vstu/compprehension/controllers/QuestionBankController.java
+++ modules/server/src/main/java/org/vstu/compprehension/controllers/QuestionBankController.java
@@ -3,36 +3,43 @@ package org.vstu.compprehension.controllers;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.AuthorizationServiceException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.vstu.compprehension.Service.UserService;
 import org.vstu.compprehension.dto.QuestionBankSearchRequestDto;
 import org.vstu.compprehension.models.businesslogic.QuestionRequest;
 import org.vstu.compprehension.models.businesslogic.domains.DomainFactory;
 import org.vstu.compprehension.models.businesslogic.storage.AbstractRdfStorage;
+import org.vstu.compprehension.models.entities.EnumData.Role;
 import org.vstu.compprehension.models.entities.EnumData.RoleInExercise;
-import org.vstu.compprehension.models.repository.QuestionMetadataRepository;
 
 @Controller
 @RequestMapping("api/question-bank")
 @Log4j2
 public class QuestionBankController {
-    private final QuestionMetadataRepository metadataRepository;
     private final DomainFactory domainFactory;
     private final AbstractRdfStorage questionStorage;
+    private final UserService userService;
 
     @Autowired
-    public QuestionBankController(QuestionMetadataRepository metadataRepository, DomainFactory domainFactory, AbstractRdfStorage questionStorage) {
-        this.metadataRepository = metadataRepository;
-        this.domainFactory      = domainFactory;
+    public QuestionBankController(DomainFactory domainFactory, AbstractRdfStorage questionStorage, UserService userService) {
+        this.domainFactory   = domainFactory;
         this.questionStorage = questionStorage;
+        this.userService     = userService;
     }
 
     @RequestMapping(value = {"count"}, method = { RequestMethod.POST }, produces = "application/json", consumes = "application/json")
     @ResponseBody
     public Integer getQuestionsCount(@RequestBody QuestionBankSearchRequestDto searchRequest, HttpServletRequest request) throws Exception {
+        var currentUser = userService.getCurrentUser();
+        if (!currentUser.getRoles().contains(Role.TEACHER)) {
+            throw new AuthorizationServiceException("Unathorized");
+        }        
+        
         var domain = domainFactory.getDomain(searchRequest.getDomainId());
 
         var targetConcepts = searchRequest.getConcepts().stream()
