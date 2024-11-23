7b6fc711a2a578a69fba0c5a8da9e89c64cfe67b
Artem Prokudin <prokudintema@yandex.ru>
2024-11-23 21:17:37 +0300

    remove unused domain options

--- modules/background-server/src/main/java/org/vstu/compprehension/jobs/metadatahealth/MetadataHealthJob.java
+++ modules/background-server/src/main/java/org/vstu/compprehension/jobs/metadatahealth/MetadataHealthJob.java
@@ -2,21 +2,12 @@ package org.vstu.compprehension.jobs.metadatahealth;
 
 import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
-import org.apache.logging.log4j.Level;
 import org.jobrunr.jobs.annotations.Job;
 import org.springframework.stereotype.Service;
-import org.vstu.compprehension.models.entities.DomainEntity;
 import org.vstu.compprehension.models.entities.QuestionGenerationRequestEntity;
-import org.vstu.compprehension.models.entities.QuestionMetadataEntity;
 import org.vstu.compprehension.models.repository.DomainRepository;
 import org.vstu.compprehension.models.repository.QuestionGenerationRequestRepository;
 import org.vstu.compprehension.models.repository.QuestionMetadataRepository;
-
-import java.net.URI;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.stream.Collectors;
 
 @Log4j2
 @Service
@@ -45,60 +36,7 @@ public class MetadataHealthJob {
 
     @SneakyThrows
     private synchronized void runImpl() {
-        //checkMetadataHealth();
         removeTooOldGenerationRequestStats();
-    }
-
-    @SneakyThrows
-    private synchronized void checkMetadataHealth() {
-        log.info("Starting metadata health job. Mode: {}", config.getMode());
-        
-        var domains = domainRep.findAll()
-                .stream()
-                .collect(Collectors.toMap(DomainEntity::getShortName, domain -> domain));
-        
-        int pageSize     = 5_000;        
-        long total       = metadataRep.count();
-        int processed    = 0;
-        int deleted      = 0;
-        int lastLoadedId = 0;
-        while (true) {
-            var metadataChunk = metadataRep.loadPage(lastLoadedId, pageSize);
-            if (metadataChunk.isEmpty()) {
-                break;
-            }
-            lastLoadedId = metadataChunk.getLast().getId();
-
-            var toDelete = new ArrayList<QuestionMetadataEntity>();
-            for (var metadata : metadataChunk) {
-                var domain = domains.get(metadata.getDomainShortname());
-                if (domain == null) {
-                    log.error("Domain not found: {} for metadata with id: {}", metadata.getDomainShortname(), metadata.getId());
-                    continue;
-                }
-                
-                var fullMetadataPath = Path.of(Paths.get(new URI(domain.getOptions().getStorageDownloadFilesBaseUrl())).toAbsolutePath().toString(), metadata.getQDataGraph());
-                if (fullMetadataPath.toFile().exists()) {
-                    continue;
-                }
-                
-                log.warn("Question file not found for metadata {} with path: {}", metadata.getId(), fullMetadataPath);
-                toDelete.add(metadata);
-            }
-
-            if (!toDelete.isEmpty()) {
-                if (config.getMode() == MetadataHealthJobConfig.Mode.DELETE_INVALID) {
-                    metadataRep.deleteAll(toDelete);
-                    deleted += toDelete.size();
-                    log.info("Deleted {} metadata entities", toDelete.size());
-                }
-            }
-
-            processed += metadataChunk.size();
-            log.printf(Level.INFO, "Processed %d/%d (%2.2f%%) metadata entities", processed, total, 100f * processed / total);
-        }
-        
-        log.info("Finished metadata health job. Deleted {} metadata entities", deleted);
     }
     
     private void removeTooOldGenerationRequestStats() {
