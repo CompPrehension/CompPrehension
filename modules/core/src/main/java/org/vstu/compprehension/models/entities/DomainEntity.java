5b4a9ec9fd8beed4fa8a4e8bf13f7302a85a9b15
Dyatiend <readysteel@ya.ru>
2025-04-24 14:17:17 +0300

    Добавил поле для бкт модели в бд

--- modules/core/src/main/java/org/vstu/compprehension/models/entities/DomainEntity.java
+++ modules/core/src/main/java/org/vstu/compprehension/models/entities/DomainEntity.java
@@ -2,6 +2,7 @@ package org.vstu.compprehension.models.entities;
 
 import com.vladmihalcea.hibernate.type.json.JsonType;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.Type;
@@ -9,8 +10,18 @@ import org.hibernate.annotations.Type;
 @Entity
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "Domain", uniqueConstraints = @UniqueConstraint(columnNames = {"name", "version"}))
 public class DomainEntity {
+
+    public DomainEntity(DomainEntity other) {
+        setName(other.getName());
+        setShortName(other.getShortName());
+        setVersion(other.getVersion());
+        setOptions(other.getOptions());
+        setBktRoster(other.getBktRoster());
+    }
+
     @Id
     @Column(name = "name")
     private String name;
@@ -24,4 +35,7 @@ public class DomainEntity {
     @Type(JsonType.class)
     @Column(name = "options_json", columnDefinition = "json", nullable = false)
     private DomainOptionsEntity options;
+
+    @Column(name = "bkt_roster", columnDefinition = "TEXT")
+    private String bktRoster;
 }
