b8b289a9c63e2f6f380bdd692ec2a26b7bfc7854
Den1s0v M <hello-lumen@ya.ru>
2025-07-11 20:30:58 +0300

    fix(ui): Term detection improvements

--- modules/core/src/main/java/org/vstu/compprehension/models/businesslogic/domains/terms/methods/FuzzyTermDetectionMethod.java
+++ modules/core/src/main/java/org/vstu/compprehension/models/businesslogic/domains/terms/methods/FuzzyTermDetectionMethod.java
@@ -1,5 +1,6 @@
 package org.vstu.compprehension.models.businesslogic.domains.terms.methods;
 
+import lombok.AllArgsConstructor;
 import org.apache.commons.lang3.tuple.Pair;
 import org.vstu.compprehension.models.businesslogic.domains.terms.DomainTermAnnotation;
 import org.vstu.compprehension.models.businesslogic.domains.terms.DomainTermDetectionMethod;
@@ -17,8 +18,11 @@ import static org.vstu.compprehension.common.StringHelper.stripTags;
 import static org.vstu.compprehension.models.businesslogic.domains.terms.utils.TagTolerantStringTokenizer.findWordPosition;
 
 public class FuzzyTermDetectionMethod implements DomainTermDetectionMethod {
-
-    public record MatchResult(String text, int score, int pos, int length) { }
+    
+    @AllArgsConstructor
+    public static class MatchResult {
+        String text; int score; int pos; int length;
+    }
 
     public static List<MatchResult> fuzzyNgramSearch(String text, String pattern, int baseMaxScore) {
         String[] words = TagTolerantStringTokenizer.tokenize(text);
@@ -38,6 +42,7 @@ public class FuzzyTermDetectionMethod implements DomainTermDetectionMethod {
             int score = (int)(WeightedSimilarity.computeWRatio(stripTags(ngram), pattern) * 100);
 
             if (score >= baseMaxScore) {
+                // TODO: вместо разбора текста заново взять слова из words ?
                 int offset = findWordPosition(text, posKey.getKey())[0];
                 int length = findWordPosition(text, posKey.getValue())[0] - offset - 1;
                 if (offset != -1) {
@@ -56,9 +61,32 @@ public class FuzzyTermDetectionMethod implements DomainTermDetectionMethod {
         if (threshold < 1) {
             threshold *= 100;
         }
-        return fuzzyNgramSearch(source, element.getPattern(language),
-                (int)threshold
-                ).stream().sorted(Comparator.comparingInt(o -> -o.score))
+        List<MatchResult> matches_found = fuzzyNgramSearch(source, element.getPattern(language),
+                (int) threshold
+        );
+
+        // Убрать излишнюю, т.е. не присутствующую в паттерне, пунктуацию (1+ символов с конца)
+        String pattern = element.getPattern(language);
+        for (var match : matches_found) {
+            char pattern_last_char = pattern.charAt(pattern.length() - 1);
+            for (int last_pos = match.pos + match.length - 1; last_pos >= 0; --last_pos) {
+                char last_char = source.charAt(last_pos);
+                var c_type = Character.getType(last_char);
+                if (c_type == Character.END_PUNCTUATION || c_type == Character.DASH_PUNCTUATION || c_type == Character.OTHER_PUNCTUATION) {
+                    if (last_char != pattern_last_char) {
+                        // Вырезать 1 символ в конце, т.к. он является неожиданной пунктуацией
+                        match.length -= 1;
+                        ///
+                        System.out.println("\nВырезать 1 символ в конце: " +source.substring(match.pos, last_pos + 1)+ '\n');
+                        ///
+                        continue;
+                    }
+                }
+                break;  // No changes, stop iteration.
+            }
+        }
+
+        return matches_found.stream().sorted(Comparator.comparingInt(o -> -o.score))
                 .map(match -> new DomainTermAnnotation(
                         element, language,
                         match.pos, match.length
