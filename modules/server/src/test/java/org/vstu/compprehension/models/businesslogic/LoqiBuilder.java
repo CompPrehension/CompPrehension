d78c960290a9bd3274484ad20f0968cf37b059a0
Artem Prokudin <prokudintema@yandex.ru>
2025-12-01 22:41:53 +0300

    + fix

--- modules/server/src/test/java/org/vstu/compprehension/models/businesslogic/LoqiBuilder.java
+++ modules/server/src/test/java/org/vstu/compprehension/models/businesslogic/LoqiBuilder.java
@@ -69,7 +69,7 @@ public class LoqiBuilder {
         exercise.setTags("");
         exercise.setOptions(new ExerciseOptionsEntity(null, true,
                 true, true, true, true,
-                true, 7));
+                true, 7, null));
         exercise.setName("test");
         exercise.setStages(Collections.singletonList(new ExerciseStageEntity()));
         exercise.setStrategyId("StaticStrategy");
