5d7d6d051aaa82fd80cccaf0cc0202c846423aef
den1s0v <hello-lumen@ya.ru>
2025-12-14 02:53:57 +0300

    chore: use dev profile

--- modules/server/src/test/java/org/vstu/compprehension/models/businesslogic/dt/CtrlFlowDTTest.java
+++ modules/server/src/test/java/org/vstu/compprehension/models/businesslogic/dt/CtrlFlowDTTest.java
@@ -32,7 +32,7 @@ import java.util.stream.StreamSupport;
 
 @SpringBootTest
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-@ActiveProfiles("test")
+@ActiveProfiles("dev")
 @Transactional
 public class CtrlFlowDTTest {
     @Autowired
