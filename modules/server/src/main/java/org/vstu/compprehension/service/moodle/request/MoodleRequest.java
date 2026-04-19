46fd79bec16440e1f54d627c5588a99a1691683f
dmitry-smirnov <saucechilidev@gmail.com>
2026-04-19 21:02:42 +0300

    del: отказываемся(временно) от отправки оценки без LTI

--- modules/server/src/main/java/org/vstu/compprehension/service/moodle/request/MoodleRequest.java
+++
@@ -1,7 +0,0 @@
-package org.vstu.compprehension.service.moodle.request;
-
-/**
- * Маркерный интерфейс для типизированных запросов к Moodle Web Services API.
- */
-public interface MoodleRequest {
-}
