78f57073734bb2d5cba59f17e0c1772f9a5ccbb6
dmitry-smirnov <saucechilidev@gmail.com>
2026-05-17 23:20:28 +0300

    perf: устранение N+1 запросов в списке курсов и членствах

--- modules/core/src/main/java/org/vstu/compprehension/models/repository/ExerciseCourseLinkRepository.java
+++ modules/core/src/main/java/org/vstu/compprehension/models/repository/ExerciseCourseLinkRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.vstu.compprehension.dto.course.CourseDto;
 import org.vstu.compprehension.models.entities.course.ExerciseCourseLinkEntity;
 import org.vstu.compprehension.models.entities.course.ExerciseCourseLinkId;
 
@@ -27,6 +28,16 @@ public interface ExerciseCourseLinkRepository extends JpaRepository<ExerciseCour
     int createIfAbsent(@Param("exerciseId") long exerciseId, @Param("courseId") long courseId);
 
     List<ExerciseCourseLinkEntity> findAllByExerciseId(long exerciseId);
+
+    @Query("""
+            select new org.vstu.compprehension.dto.course.CourseDto(
+                ecl.course.id, ecl.course.name,
+                ecl.course.educationResource.id, ecl.course.educationResource.url
+            )
+            from ExerciseCourseLinkEntity ecl
+            where ecl.exercise.id = :exerciseId
+            """)
+    List<CourseDto> findCourseDtosByExerciseId(@Param("exerciseId") long exerciseId);
 
     @Modifying(clearAutomatically = true)
     void deleteByExerciseIdAndCourseId(long exerciseId, long courseId);
