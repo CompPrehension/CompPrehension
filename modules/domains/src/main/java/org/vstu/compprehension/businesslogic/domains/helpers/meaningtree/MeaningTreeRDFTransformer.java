10358166a901d69456bb1f332ef34555daf39479
Artem Prokudin <prokudintema@yandex.ru>
2026-09-09 16:23:43 +0300

    disable loqi debug dumps

--- modules/domains/src/main/java/org/vstu/compprehension/businesslogic/domains/helpers/meaningtree/MeaningTreeRDFTransformer.java
+++ modules/domains/src/main/java/org/vstu/compprehension/businesslogic/domains/helpers/meaningtree/MeaningTreeRDFTransformer.java
@@ -130,7 +130,7 @@ public class MeaningTreeRDFTransformer {
                 baseTokensToTokens
         );
         situationDomain.validateAndThrow();
-        debugDumpLoqi(situationDomain, "out.loqi", domainModel);
+        // debugDumpLoqi(situationDomain, "out.loqi", domainModel);
         return parseResult.domainModel;
     }
 
